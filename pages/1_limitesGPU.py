1dbec641c0ba9c75a56dd655e39bc9f22aa2ffdc
Luis <luisoliveiraecontato@gmail.com>
2025-09-01 11:39:11 -0300

    fixing dotenv

--- pages/1_limitesGPU.py
+++ pages/1_limitesGPU.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import pandas as pd
-from dotenv import load_dotenv
-
-import os
-
-load_dotenv()
 
 st.set_page_config(layout="wide")
 st.title("Visualizador de Planilhas")
